77bf64a2e368930e9c51b84f33e87e16fa96e07e
Andrzej Rafalski <anraf1001@gmail.com>
2020-07-19 16:26:17 +0200

    Added Ship class

---
+++ ship.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <string>
+
+class Ship {
+public:
+    int getId() const { return id_; }
+    std::string getName() const { return name_; }
+    size_t getSpeed() const { return speed_; }
+    size_t getMaxCrew() const { return maxCrew_; }
+    size_t getCapacity() const { return capacity_; }
+
+private:
+    int id_;
+    std::string name_;
+    size_t speed_;
+    size_t maxCrew_;
+    size_t capacity_;
+};
