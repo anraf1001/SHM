8d37425ecd590c8f4fa66f8afe953862e92df02c
Andrzej Rafalski <anraf1001@gmail.com>
2020-07-19 16:44:19 +0200

    Added Ship filling test

--- ship-ut.cpp
+++ ship-ut.cpp
@@ -1,2 +1,12 @@
 #include "gtest/gtest.h"
 #include "ship.hpp"
+
+TEST(ShipTest, shouldFillSpecifiedParametersInShip) {
+    Ship ship(2, "ship", 4, 5, 7);
+
+    EXPECT_EQ(ship.getId(), 2);
+    EXPECT_EQ(ship.getName(), "ship");
+    EXPECT_EQ(ship.getSpeed(), 4);
+    EXPECT_EQ(ship.getMaxCrew(), 5);
+    EXPECT_EQ(ship.getCapacity(), 7);
+}
