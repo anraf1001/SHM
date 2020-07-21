a97859ea108fae7fec0588f03561007780ee9d19
Andrzej Rafalski <anraf1001@gmail.com>
2020-07-21 11:19:45 +0200

    Deleted redundant tests

--- ship-ut.cpp
+++ ship-ut.cpp
@@ -2,25 +2,6 @@
 
 #include "ship.hpp"
 
-TEST(ShipTest, shouldFillSpecifiedParametersInShip) {
-    Ship ship(2, "ship", 4, 5, 7);
-
-    EXPECT_EQ(ship.getId(), 2);
-    EXPECT_EQ(ship.getName(), "ship");
-    EXPECT_EQ(ship.getSpeed(), 4);
-    EXPECT_EQ(ship.getMaxCrew(), 5);
-    EXPECT_EQ(ship.getCrew(), 0);
-    EXPECT_EQ(ship.getCapacity(), 7);
-}
-
-TEST(ShipTest, shouldSetName) {
-    Ship ship;
-
-    ship.setName("Ship");
-
-    EXPECT_EQ(ship.getName(), "Ship");
-}
-
 TEST(ShipTest, shouldAddAndSubtractCrew) {
     Ship ship(2, "ship", 4, 5, 7);
 
