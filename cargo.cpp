40743fd668da7920d80a44574c9d7c7cb76ab65d
Andrzej Rafalski <anraf1001@gmail.com>
2020-07-21 20:22:18 +0200

    Added operator== to Cargo

--- cargo.cpp
+++ cargo.cpp
@@ -9,3 +9,7 @@ Cargo& Cargo::operator-=(size_t amount) {
     amount_ -= amount;
     return *this;
 }
+
+bool Cargo::operator==(const Cargo& cargo) const {
+    return amount_ == cargo.amount_ && name_ == cargo.name_ && basePrice_ == cargo.basePrice_;
+}
