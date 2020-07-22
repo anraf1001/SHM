cec7262124667c78185488b765c8f4eabd613d23
Andrzej Rafalski <anraf1001@gmail.com>
2020-07-22 17:19:28 +0200

    Comparison operator for Island

--- island.cpp
+++ island.cpp
@@ -1,1 +1,7 @@
 #include "island.hpp"
+
+Island::Island(size_t x, size_t y) : position_(x, y) {}
+
+bool Island::operator==(const Island& island) const {
+    return position_ == island.position_;
+}
--- island.hpp
+++ island.hpp
@@ -4,7 +4,11 @@
 
 class Island {
 public:
+    Island(size_t x, size_t y);
+
     Coordinates getPosition() const { return position_; }
+
+    bool operator==(const Island& island) const;
 
 private:
     Coordinates position_;
