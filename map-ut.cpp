2295efeb8ad2d2478170d7c5cb050f0b1b8468a6
Andrzej Rafalski <anraf1001@gmail.com>
2020-07-22 17:19:14 +0200

    Map tests

--- map-ut.cpp
+++ map-ut.cpp
@@ -1,3 +1,22 @@
 #include <gtest/gtest.h>
 
+#include <algorithm>
+
 #include "map.hpp"
+
+struct MapTest : public ::testing::Test {
+    Map map;
+
+    bool areIslandsUnique() {
+        auto islands = map.getIslands();
+        return std::unique(islands.begin(), islands.end()) == islands.end();
+    }
+};
+
+TEST_F(MapTest, shouldGenerate10IslandsOnTheMap) {
+    ASSERT_EQ(map.getIslands().size(), 10);
+}
+
+TEST_F(MapTest, shouldGenerateUniqueIslands) {
+    ASSERT_TRUE(areIslandsUnique());
+}
