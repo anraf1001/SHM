81010639b79f66573ef8a38f2ec7e4c618152296
Andrzej Rafalski <anraf1001@gmail.com>
2020-07-22 17:19:04 +0200

    Map generating

--- map.cpp
+++ map.cpp
@@ -1,1 +1,28 @@
 #include "map.hpp"
+
+#include <algorithm>
+#include <numeric>
+#include <random>
+
+Map::Map() {
+    islands_.reserve(SIZE_OF_MAP);
+    generateMap();
+}
+
+void Map::generateMap() {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+
+    std::vector<size_t> positionX(MAP_WIDTH);
+    std::vector<size_t> positionY(MAP_HEIGHT);
+
+    std::iota(positionX.begin(), positionX.end(), 0);
+    std::iota(positionY.begin(), positionY.end(), 0);
+
+    std::shuffle(positionX.begin(), positionX.end(), gen);
+    std::shuffle(positionY.begin(), positionY.end(), gen);
+
+    for (int i = 0; i < SIZE_OF_MAP; i++) {
+        islands_.emplace_back(positionX[i], positionY[i]);
+    }
+}
--- map.hpp
+++ map.hpp
@@ -5,7 +5,18 @@
 #include "island.hpp"
 
 class Map {
+public:
+    Map();
+
+    const std::vector<Island>& getIslands() const { return islands_; }
+
+    constexpr static size_t SIZE_OF_MAP = 10;
+    constexpr static size_t MAP_WIDTH = 50;
+    constexpr static size_t MAP_HEIGHT = 50;
+
 private:
+    void generateMap();
+
     std::vector<Island> islands_;
     Island* currentPos_;
 };
